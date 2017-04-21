c9b8c1b81e6b0ec9b366eb200b0b30583cdd3d44
RTK99 <richrancy@gmail.com>
2017-04-21 08:18:49 -0400

    Main node program

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('MzA0NjI5OTUzMDU0NTA3MDE4.C9raMg.n6MCux9Vr05nNNlYY3PR_8Z5dHQ');
